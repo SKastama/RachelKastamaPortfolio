0dfb2402eb5cbb26b1b74a59a9752318d17bf397
Sarah Kastama <sarahkastama@gmail.com>
2022-02-21 16:38:06 -0800

    navigation styling

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -2,20 +2,23 @@ import React from 'react';
 import {
     Link
 } from "react-router-dom";
-import { AppBar, Toolbar, Typography} from '@mui/material';
+import { AppBar, Box, Toolbar, Typography} from '@mui/material';
 
 const NavBar = () => {
     return (
         <AppBar position='sticky' sx={{ width: 'auto'}}>
-            <Toolbar>
-                <Typography variant='h4'>Rachel Kastama</Typography>
-                <div>
-                    <Link to='/' style={{margin: '1em'}}>Home</Link>
-                    <Link to='/projects' style={{margin: '1em'}}>Projects</Link>
-                    <Link to='/resume' style={{margin: '1em'}}>Resume</Link>
-                    <Link to='/about' style={{margin: '1em'}}>About</Link>
-                </div>
-            </Toolbar>
+            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', paddingY: '2em', paddingX: '5em'}}>
+                <Box sx={{ width: 'auto'}}>
+                    <Typography variant='h5'>Rachel Kastama</Typography>
+                    <hr />
+                </Box>
+                <Box>
+                    <Link to='/' style={{textDecoration: 'none'}}><Typography variant='h7' color='common.white' sx={{ marginRight: '1em'}}>Home</Typography></Link>
+                    <Link to='/projects' style={{textDecoration: 'none'}}><Typography variant='h7' color='common.white' sx={{ marginRight: '1em'}}>Projects</Typography></Link>
+                    <Link to='/resume' style={{textDecoration: 'none'}}><Typography variant='h7' color='common.white' sx={{ marginRight: '1em'}}>Resume</Typography></Link>
+                    <Link to='/about' style={{textDecoration: 'none'}}><Typography variant='h7' color='common.white'>About</Typography></Link>
+                </Box>
+            </Box>
         </AppBar>
     )
 }
